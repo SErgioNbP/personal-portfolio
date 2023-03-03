b590c57955cd31dd7407da765f74a56551f3c181
SErgioNbP <sergionevespinheiro@gmail.com>
2023-03-03 16:53:05 +0000

    small changes added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import vitePluginFaviconsInject from 'vite-plugin-favicons-inject';
 
 export default defineConfig({
   plugins:
     [
       react(),
-      vitePluginFaviconsInject('./src/assets/favicon.png'),
     ],
 });
